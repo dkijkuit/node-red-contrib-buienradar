058bf3b77f8e1974d01e971e38a238faf7debaf4
dkijkuit <dkijkuit@gmail.com>
2016-08-06 08:11:36 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+node-red-contrib-buienradar
+============================
+
+A <a href="http://nodered.org" target="_new">Node-RED</a> node that gets the
+weather from buienradar.nl.
+
+Install
+-------
+
+Run the following command in the root directory of your Node-RED install
+
+        npm install node-red-contrib-buienradar
+
+Usage
+-----
+
+Only one node with an output is available. More to come in near future. Only use this for personal use. This node depends on the data from buienradar.nl, for commercial use of their service please contact them directly. I am not related to buienradar.nl in any way.
+
+
+### Buienradar Node
+
+Fetches the current weather for the selected weather station and polls it for the specified interval.
+Outputs a **msg.payload.buienradar** object which holds all the data.
+
+### Results
+
+Current conditions will return
+
+- **stationcode** - unique station code which corresponds to a weather station
+- **stationnaam** - the weather station name
+- **temperatuurGC** - temperature in degrees celsius
+- **windsnelheidBF** - windspeed in beaufort
+- **luchtvochtigheid** - humidity in percentage
+- **datum** - the datetime of the measurement
+- **zichtmeters** - sight in meters
+- **icoonactueel** - an URL to the current weather icon
+- **icoonzin** - short description of the current weather
+- **regenMMPU** - rain in millimeters per hour
+- **luchtdruk** - air pressure
+- **windrichtingGR** - wind direction in degrees
+- **windrichting** - wind direction
+
+Weather data provided by <a href="http://www.buienradar.nl/" target="_blank">Buienradar</a>
---
+++ buienradar.js
@@ -0,0 +1,126 @@
+module.exports = function(RED) {
+    var xpath = require("xpath");
+    var xmldom = require("xmldom").DOMParser;
+    var http = require("http");
+
+    function BuienradarNode(config) {
+        RED.nodes.createNode(this,config);
+
+        var node = this;
+        node.interval_id = null;
+
+        if(config.interval){
+			node.interval = config.interval;
+		}
+
+        if(config.station){
+			node.station = config.station;
+		}
+
+        //node.log("Buienradar station set to: "+node.station+", config: "+config.station);
+
+        if(node.interval!="manual"){
+            this.interval_id = setInterval( function() {
+                node.emit("input",{});
+            }, node.interval);
+        }
+
+        this.on('input', function(msg) {
+            buienradarPoll(node, msg, function(err){
+                node.send(msg);
+            });
+        });
+
+        this.on("close", function() {
+            if (this.interval_id !== null) {
+                clearInterval(this.interval_id);
+            }
+        });
+
+        node.emit("input",{});
+    }
+
+    function buienradarPoll(node, msg, callback) {
+        //wipe clear the payload if it exists, or create it if it doesn't
+        msg.payload = {};
+        msg.payload.buienradar = {};
+
+        //node.log("Requesting buienradar update ...");
+
+        var url = "http://xml.buienradar.nl";
+        if (url) {
+            node.status({fill:"blue",shape:"dot",text:"Ophalen weergegevens"});
+            var req = http.get(url, function(res) {
+                // save the data
+                var xml = '';
+                res.on('data', function(chunk) {
+                    xml += chunk;
+                });
+
+                res.on('end', function() {
+                    //node.log("Incoming xml size: "+xml.length);
+                    var doc = new xmldom().parseFromString(xml)
+                    var nodes = xpath.select("//weerstation[@id=\""+node.station+"\"]", doc);
+
+                    //console.log(nodes[0].localName + ": " + nodes[0].firstChild.data);
+                    var childNodes = nodes[0].childNodes;
+
+                    for(var i=0; i<childNodes.length; i++){
+                        var childNode = childNodes[i];
+                        //console.log(childNode.localName + ": " + childNode.firstChild.data);
+                        switch(childNode.localName){
+                            case "stationcode":
+                                msg.payload.buienradar.stationcode = childNode.firstChild.data;
+                                break;
+                            case "stationnaam":
+                                msg.payload.buienradar.stationnaam = childNode.firstChild.data;
+                                break;
+                            case "temperatuurGC":
+                                msg.payload.buienradar.temperatuurGC = childNode.firstChild.data;
+                                break;
+                            case "windsnelheidBF":
+                                msg.payload.buienradar.windsnelheidBF = childNode.firstChild.data;
+                                break;
+                            case "luchtvochtigheid":
+                                msg.payload.buienradar.luchtvochtigheid = childNode.firstChild.data;
+                                break;
+                            case "datum":
+                                msg.payload.buienradar.datum = childNode.firstChild.data;
+                                break;
+                            case "zichtmeters":
+                                msg.payload.buienradar.zichtmeters = childNode.firstChild.data;
+                                break;
+                            case "icoonactueel":
+                                msg.payload.buienradar.icoonactueel = childNode.firstChild.data;
+                                msg.payload.buienradar.icoonzin = childNode.getAttribute('zin');
+                                break;
+                            case "regenMMPU":
+                                msg.payload.buienradar.regenMMPU = childNode.firstChild.data;
+                                break;
+                            case "luchtdruk":
+                                msg.payload.buienradar.luchtdruk = childNode.firstChild.data;
+                                break;
+                            case "windrichtingGR":
+                                msg.payload.buienradar.windrichtingGR = childNode.firstChild.data;
+                                break;
+                            case "windrichting":
+                                msg.payload.buienradar.windrichting = childNode.firstChild.data;
+                                break;
+                            default:
+                                break;
+                        }
+                    }
+
+                    callback();
+                });
+
+                req.on('error', function(err) {
+                    callback(err);
+                    node.log("Failed: "+err);
+                });
+            });
+            node.status({});
+        }
+    }
+    RED.nodes.registerType("buienradar", BuienradarNode);
+}
